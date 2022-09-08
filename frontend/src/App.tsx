76976a20ced177f96bf82145717371a98b094ecd
Nelkisa Matias <gabrielamatias60@gmail.com>
2022-09-07 23:06:47 -0300

    componente notifyButton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>opa</h1>
+    <>
+      <h1>opa</h1>
+      <NotificationButton />
+    </>
   )
 }
 
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css'
+import icon from '../../assets/img/icon.svg'
+
+function NotificationButton() {
+    return (
+        <div className="notify-button">
+            <img src={icon} alt="aaa" />
+        </div>
+
+    )
+}
+
+export default NotificationButton
